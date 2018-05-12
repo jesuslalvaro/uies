93b1bb4cf00f777c70f98902a48be5253fb63539
jesuslalvaro <jesuslalvaro@gmail.com>
2018-05-12 13:02:42 +0000

    NUevos scripts de instalacion de grub (tanto classic como EFI), instalacion de archivos y generador de menus

---
+++ bin/grubFD.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+
+###   instala las dos versiones de grub (clasica y EFI) 
+###   y escribe un menu inicial
+###
+###   correr como sudo
+### ejemplo
+### sudo uies/bin/grubFolderDevice.sh /media/ubuntu-studio/LLINUX /dev/sde
+
+
+## pendrive formateado como vfat fat32
+#FOLDER=/media/ubuntu-studio/LLINUX
+#DEVICE=/dev/sdd
+
+FOLDER=$1  
+DEVICE=$2  
+
+apt-get install grub-efi-amd64
+
+##grub-install --target=x86_64-efi --efi-directory=/media/ubuntu-studio/LLINUX/ --bootloader-id=GRUB --root-directory=/media/ubuntu-studio/LLINUX/  /dev/sdd
+## grub-install  --root-directory=/media/ubuntu-studio/LLINUX/  /dev/sdd
+
+
+## doble instalacion de grub: clasica y EFI
+grub-install --target=x86_64-efi --efi-directory=$FOLDER --bootloader-id=GRUB --root-directory=$FOLDER --force $DEVICE
+grub-install --root-directory=$FOLDER  $DEVICE
+
+
+### prepara menu grub.cfg con autodeteccion de menus
+CFG=$FOLDER/boot/grub/grub.cfg
+rm $CFG
+
+cat > $CFG << EOF
+     
+insmod chain
+insmod png
+insmod part_msdos
+insmod fat
+insmod ntfs
+insmod syslinuxcfg
+insmod cpuid
+insmod ext2
+insmod all_video
+insmod configfile
+insmod normal
+insmod linux
+insmod echo
+insmod search
+insmod regexp
+
+insmod font
+if loadfont /boot/grub/fonts/unicode.pf2 ; then
+#if loadfont unicode ; then
+	# Use shift key to avoid loading gfxterm
+	if keystatus --shift ; then true ; else
+        if [ "\${grub_platform}" == "efi" ]; then
+            insmod efi_gop
+            insmod efi_uga
+        else
+            insmod vbe
+            insmod vga
+            set gfxmode=auto
+            set gfxpayload=auto
+            terminal_output gfxterm 
+            if terminal_output gfxterm ; then true ; else
+                terminal gfxterm
+            fi
+        fi
+        insmod gfxterm
+	fi
+fi
+
+set color_normal=white/black
+set color_highlight=white/light-blue
+export color_normal
+export color_highlight
+
+set default=0
+set timeout=15
+
+### intentando menus automaticos:
+for menufile in /*/menu.cfg; do
+  ##  source "\$cfgfile"
+  menuentry "\${menufile}" {
+	configfile "\$menufile"
+	}
+done
+
+EOF
+
+## para escribir en el sistema,
+## para actualizar, para parches
+## usar 
+## sudo mount  -o remount,rw /cdrom
---
+++ bin/installSKINF.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+### instala entorno live desde una carpeta casper con squashfs
+
+
+
+### ejemplo
+### uies/bin/installSKINF.sh '/home/ubuntu-studio/ll/casper' '/home/ubuntu-studio/ll/casper/vmlinuz' '/home/ubuntu-studio/ll/casper/initrd.lz' llinux '/media/ubuntu-studio/LLINUX' 
+
+SQUASH=$1  # carpeta con squash
+KERNEL=$2   
+INIT=$3    
+NAME=$4  ##NAME=bionicstudio
+FOLDER=$5  # carpeta donde instalar 
+
+#####
+
+USER=me
+HOST=$NAME
+
+####
+mkdir -p $FOLDER/$NAME/livemedia
+
+### COPIAS
+
+cp -uv $KERNEL $FOLDER/$NAME/
+cp -uv $INIT $FOLDER/$NAME/
+cp -uv $SQUASH/*.squashfs $FOLDER/$NAME/livemedia/
+
+
+#### MENU ###
+MENU=$FOLDER/$NAME/menu.cfg
+rm $MENU
+
+cat > $MENU << EOF
+
+set default=0
+set timeout=30
+
+spanish="console-setup/layoutcode=es locale=es_ES debian-installer/language=es keyboard-configuration/layoutcode=es "
+custom="username=$USER hostname=$HOST"
+
+menuentry "$NAME" {
+	set gfxpayload=keep
+	linux	/$NAME/$(basename "${KERNEL}")  live-media-path=/$NAME/livemedia  \${spanish} \${custom} file=/cdrom/preseed/xubuntu.seed boot=casper  quiet splash ignore_bootid cdrom-detect/try-usb=true floppy.allowed_drive_mask=0 ignore_uuid root=UUID=DA22-6A73 ---
+	initrd	/$NAME/$(basename "${INIT}")
+}
+
+EOF
+
+## para escribir en el sistema,
